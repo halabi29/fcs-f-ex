ea8a0eeae86826dceddb24b34cdd695f32431f01
halabi29 <151402435+halabi29@users.noreply.github.com>
2025-11-02 11:49:02 +0200

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+const student = {
+    name: "Nassim",
+    score: 74
+};
+console.log(student);
+
+
+class Phone {
+    call() {
+    }
+}
